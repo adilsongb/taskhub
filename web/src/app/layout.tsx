42f90eba7801f39917d2fba8bf2a9c412bf0e757
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2024-01-06 19:54:27 -0300

    feat: add styled components

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import StyledComponentsRegistry from './registry'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
